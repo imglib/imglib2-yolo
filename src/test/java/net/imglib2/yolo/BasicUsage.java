67e20e1e8ee0c86d11c07376df6ee977842c9589
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-06-20 13:53:17 +0200

    Improve demo.

--- src/test/java/net/imglib2/yolo/BasicUsage.java
+++ src/test/java/net/imglib2/yolo/BasicUsage.java
@@ -1,5 +1,6 @@
 package net.imglib2.yolo;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,9 @@ import org.apposed.appose.TaskException;
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
+import ij.gui.Overlay;
+import ij.gui.Roi;
+import ij.gui.TextRoi;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
@@ -43,19 +47,65 @@ public class BasicUsage
 		// Get messages about installing and processing
 		final ApposeTaskListener listener = ApposeTaskListener.STD;
 
-		// Specify the parameters for Cellpose 3
+		// Specify the parameters for YOLO
 		final YOLOParameters params = YOLOParameters.builder()
 				.builtinModel( YOLOBuiltinModels.YOLO26N )
-				.useSahi( false )
+				.useSahi( true )
 				.build();
 
 		final List< Map< String, Object > > output = YOLO.detectRGB( input, params, listener );
-
 		System.out.println( "Detected " + output.size() + " objects" );
-		for ( final Map< String, Object > detection : output )
+		showOutput( output, imp );
+	}
+
+	private static void showOutput( final List< Map< String, Object > > output, final ImagePlus imp )
+	{
+		// Prep overlay for output
+		Overlay overlay = imp.getOverlay();
+		if ( overlay == null )
 		{
-			System.out.println( detection );
+			overlay = new Overlay();
+			imp.setOverlay( overlay );
+		}
+		else
+		{
+			overlay.clear();
 		}
 
+		for ( final Map< String, Object > detection : output )
+		{
+			final double score = ( ( Number ) detection.get( "score" ) ).doubleValue();
+			final int classId = ( int ) detection.get( "class_id" );
+			final int id = ( int ) detection.get( "id" );
+			final double x1 = ( ( Number ) detection.get( "x1" ) ).doubleValue();
+			final double y1 = ( ( Number ) detection.get( "y1" ) ).doubleValue();
+			final double x2 = ( ( Number ) detection.get( "x2" ) ).doubleValue();
+			final double y2 = ( ( Number ) detection.get( "y2" ) ).doubleValue();
+			final String className = ( String ) detection.get( "class_name" );
+
+			final Roi roi = new Roi( x1, y1, x2 - x1, y2 - y1 );
+			roi.setStrokeColor( get( classId ) );
+			overlay.add( roi );
+
+			final TextRoi textRoi = new TextRoi( x1, y1 - 20, id + ": " + className + " (" + String.format( "%.2f", score ) + ")" );
+			textRoi.setFillColor( Color.BLACK );
+			textRoi.setStrokeColor( get( classId ) );
+			overlay.add( textRoi );
+		}
+
+		imp.updateAndDraw();
+		System.out.println( "Done." );
+	}
+
+	/**
+	 * Get a random color, but fixed for a given n.
+	 */
+	private static final Color get(  final int n )
+	{
+		// Forbid dark color.
+		final int r = ( ( ( 1 + n ) * 123 ) % 128 ) + 128;
+		final int g = ( ( ( 2 + n ) * 456 ) % 128 ) + 128;
+		final int b = ( ( ( 3 + n ) * 789 ) % 128 ) + 128;
+		return new Color( r, g, b );
 	}
 }
